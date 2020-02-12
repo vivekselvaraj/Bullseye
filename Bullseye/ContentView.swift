db16841d9c179f984705a0ee16460322eb60345a
Vivek Selvaraj <vivek.selvaraj@icloud.com>
2020-02-12 08:36:22 +0530

    UI elements

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible = false
+    @State var sliderValue: Double = 50.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            //Target Row
+            HStack {
+                Text("Target to Hit: ")
+                Text("100")
+            }
+            Spacer()
+            //Slider Row
+            HStack {
+                Text("1")
+                Slider(value: $sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            
+            
+            //Hit me Row
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible){ () ->
+                Alert in
+                return Alert(title: Text("Hello"), message: Text("\(Int(self.sliderValue.rounded()))"), dismissButton: Alert.Button.default(Text("Okkkaaay")))
+            }
+            Spacer()
+            //Third Row
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score: ")
+                Text("99999")
+                Spacer()
+                Text("Round: ")
+                Text("999")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Info")
+                }
+            }
+            .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
+
